57d47b5d9108360601eafffaad4cbab25f9b2d7e
Amish2417 <anishraj2417>
2024-07-25 16:55:50 +0530

    Practice-Set-26

---
+++ index.js
@@ -0,0 +1,72 @@
+  // 1
+
+  // function checkOddEven(num){
+  //   if(num%2 == 0){
+  //     console.log(`${num} is even`);
+  //   }
+  //   else{
+  //     console.log(`${num} is odd`);
+  //   }
+  // }
+  // checkOddEven(15);
+
+  // 2
+
+  // function fun(num1,num2){
+  //   if(num1>num2){
+  //     return num1
+  //   }else if(num2 > num1){
+  //     return num2;
+  //   } else{
+  //     return 'both are equal';
+  //   }
+
+  // }
+  // console.log(fun(10,10));
+
+  // 3
+
+    // function convertCelsiusToFarenhite(C){
+    //   let F = ((9*C)/5 + 32)
+    //   console.log(`Farenhite is : ${F}`);
+    // }
+    // convertCelsiusToFarenhite(37);
+
+    // 4
+
+    // function square(num){
+    //   return num*num;
+    // }
+    // console.log(square(10));
+
+    // 5
+
+    // let x = 10;
+    // let y = 20;
+    // console.log(`Before Function`)
+    // console.log(`value of x is ${x},value of y is ${y}`);
+    // function fun(x,y){
+    //   x++;
+    //   y++;
+    //   console.log(`Inside Function`)
+    //   console.log(`value of x is ${x},value of y is ${y}`);
+
+    // }
+    // fun(x,y);
+    // console.log(`After Function`)
+    // console.log(`value of x is ${x},value of y is ${y}`);
+
+    // 6
+
+    //  function getAverage(a,b,c,d,e){
+    //     return (a+b+c+d+e)/5;
+    //  }
+    //  console.log(getAverage(10,10,10,10,10));
+
+    // 7
+
+      // function concatStrings(str1,str2){
+      //   return str1+str2;
+      // }
+      // console.log(concatStrings("Anish"," Kumar"))
+   
